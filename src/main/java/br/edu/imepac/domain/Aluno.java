73572e5fa12d8607d28b9e8af0e4bcc6508dbae9
unknown <232-000755@IMEPACAD.local>
2024-09-11 22:21:49 -0300

    Application run

--- src/main/java/br/edu/imepac/domain/Aluno.java
+++ src/main/java/br/edu/imepac/domain/Aluno.java
@@ -4,5 +4,27 @@ public class Aluno {
     private String nome;
     private String matricula;
     private String email;
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getMatricula(){
+        return matricula;
+    }
+    public void setMatricula(String nome){
+        this.matricula = matricula;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+    public void setEmail(String matricula){
+        this.email = email;
+    }
 }
+
 
